66fea1831929dd05b3e496ee2cdf3823258a5666
ArthurTopazio <perereacmb@gmail.com>
2022-09-15 20:38:49 +0300

    complite basic structure

--- src/components/PostForm/PostForm.tsx
+++ src/components/PostForm/PostForm.tsx
@@ -1,7 +1,12 @@
 import { FC } from 'react';
 
+import useInput from '../../hooks/useInput';
 import { IPost } from '../../models/IPost';
 
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Chip from '@mui/material/Chip';
 import { GridElementWrapper } from '../UI/GridElementWrapper';
 
 interface PostFormTPD {
@@ -11,11 +16,36 @@ interface PostFormTPD {
 
 const PostForm: FC<PostFormTPD> = ({ postsAmount, formAction }) => {
 
-  console.log('Form props', postsAmount, formAction);
+  const postAuthor = useInput('');
+  const avatarLink = useInput('');
+  const postText = useInput('');
+
+  const handleSubmit = () => {
+
+    const newPost: IPost = {
+      author: postAuthor.value,
+      avatar_link: avatarLink.value,
+      text: postText.value,
+      create_time: String(Date.now()),
+      id: Date.now()
+    }
+
+    formAction(newPost);
+  }
 
   return (
     <GridElementWrapper sx={{ minHeight: '30vh' }}>
-      Form </GridElementWrapper>
+      <Stack direction="column" spacing={3}>
+
+        <TextField {...postAuthor} label="Your name*" variant="standard" />
+        <TextField {...avatarLink} label="Avatar link" variant="filled" />
+        <TextField {...postText} label="Post text" multiline />
+
+        <Button variant="contained" onClick={handleSubmit}> Post </Button>
+        <Chip label={`Overall: ${postsAmount}`} sx={{ width: '30%' }} />
+
+      </Stack>
+    </GridElementWrapper>
   )
 }
 
